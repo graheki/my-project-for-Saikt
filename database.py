e984523ca34cc90e227fb594f6ee26f50d9be657
graheki <svk.prygrysha@gmail.com>
2025-05-20 11:51:31 +0700

    tjy
    ftgdh

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
+import sqlite3 
+
 def add_user(username, email, password, password2):
     conn = sqlite3.connect('gallery.db')
 
     cursor = conn.cursor()
 
-    sql = "INSERT INTO user (username, email, password) VALUES (?, ?, ?)"
+    sql = "INSERT INTO user (login, email, password) VALUES (?, ?, ?)"
 
     cursor.execute(sql, (username, email, password))
 
@@ -11,3 +13,13 @@ def add_user(username, email, password, password2):
 
     conn.close()
 
+def log_in_user(username, password):
+    conn = sqlite3.connect('gallery.db')
+
+    cursor = conn.cursor()
+
+    print(cursor.fetchone(1))
+
+    conn.commit()
+
+    conn.close()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from database import add_user
+from database import add_user, log_in_user
 from flask import request
 
 
@@ -8,8 +8,15 @@ app = Flask(__name__)
 
 # Обработка маршрута
 # Обычная страница
-@app.route("/")
-def index():
+@app.route("/", methods=['GET', 'POST'])
+def index(): 
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(f"Username: {username}, Password: {password}")
+        log_in_user(username, password)
+
     return render_template("index.html")
 
 # Страница о сайте
@@ -22,7 +29,16 @@ def sing_up():
         password = request.form['password']
         password2 = request.form['password2']
         print(f"Username: {username}, Email: {email}, Password: {password} and {password2}")
-        add_user(username, email, password, password2)
+        if password != password2:
+            ...    
+        else:
+            for i in range(0, len(email)):
+                if email[i] != "@":
+                    ...
+                else:
+                    add_user(username, email, password, password2)
+                    return render_template("index.html")
+            
     return render_template("sing_up.html")
 
 @app.route("/chat")
